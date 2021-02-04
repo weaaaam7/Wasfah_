9b98f3f53ae29df7d6ac2b88bfe63cbc271a21c1
weaaaam7 <71516933+weaaaam7@users.noreply.github.com>
2021-02-04 10:42:02 +0300

    signup updated & reset password & publish (not completed yet)

---
+++ app/src/main/java/com/example/wasfah/RecipeHelperClass.java
@@ -0,0 +1,20 @@
+package com.example.wasfah;
+
+public class RecipeHelperClass {
+    private String url;
+    public RecipeHelperClass() {
+
+    }
+    public RecipeHelperClass(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
+
