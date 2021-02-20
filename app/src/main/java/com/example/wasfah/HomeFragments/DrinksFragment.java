d6a9676f25417ee7f573a229c8a111d54eccfad3
Shaden Mohammed <mcshaden@icloud.com>
2021-02-21 02:27:54 +0300

    changes in the mainActivity.

--- app/src/main/java/com/example/wasfah/HomeFragments/DrinksFragment.java
+++ app/src/main/java/com/example/wasfah/HomeFragments/DrinksFragment.java
@@ -2,22 +2,154 @@ package com.example.wasfah.HomeFragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.wasfah.Ingredients;
 import com.example.wasfah.R;
+import com.example.wasfah.RecipeInfo;
+import com.example.wasfah.RecyclerViewAdapter;
+import com.example.wasfah.Steps;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DrinksFragment extends Fragment {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("Users");
+    DatabaseReference recipeRef = database.getReference("Recipes");
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    String userID = user.getUid();
+    String name,email,username;
+    List<RecipeInfo> recipieList;
+    DataSnapshot userDataSnap;
+    String currentUser;
+    boolean isPublishedByUser=false;
     public DrinksFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_drinks, container, false);
+        View RootView = inflater.inflate(R.layout.fragment_drinks, container, false);
+        recipieList=new ArrayList<>();
+        if (user != null) {
+            // Read from the database
+
+            myRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    currentUser=dataSnapshot.child(userID).child("name").getValue(String.class);
+                    userDataSnap=dataSnapshot;
+                    recipeRef.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            getAllRecipes(dataSnapshot,userDataSnap);
+                            RecyclerView rs=(RecyclerView) RootView.findViewById(R.id.rv);
+                            if (recipieList.size()>0)
+                            {
+                                RecyclerViewAdapter recAdap = new RecyclerViewAdapter(getContext(),recipieList,currentUser);
+                                rs.setLayoutManager(new GridLayoutManager(getContext(),1));
+                                rs.setAdapter(recAdap);
+                            }
+                            else {
+                                rs.setVisibility(View.GONE);
+                                TextView tv_noRecipe=(TextView) RootView.findViewById(R.id.tv_noRecipe);
+                                tv_noRecipe.setVisibility(View.VISIBLE);
+
+
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError error) {
+                            // Failed to read value
+//                    Log.w(TAG, "Failed to read value.", error.toException());
+                        }
+                    });
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+//                    Log.w(TAG, "Failed to read value.", error.toException());
+                }
+            });
+
+
+
+
+
+            // Inflate the layout for this fragment
+
+        }
+        return RootView;
+
+    }
+    private void getAllRecipes(@NonNull DataSnapshot snapshot, @NonNull DataSnapshot userSnapShot) {
+        if (recipieList != null && recipieList.size() > 0) {
+            recipieList.clear();
+        }
+
+
+
+        for (DataSnapshot ds : snapshot.getChildren()) {
+            String Category = snapshot.child(ds.getKey()).child("category").getValue(String.class);
+            String Email = snapshot.child(ds.getKey()).child("createdBy").getValue(String.class);
+            for (DataSnapshot snap:userSnapShot.getChildren()){
+
+                email= snap.child("email").getValue(String.class);
+                if(email!=null && Email!=null && email.equalsIgnoreCase(Email))
+                {
+                    name=snap.child("name").getValue(String.class);
+                }
+
+            }
+
+
+            if (Category != null &&Category.equalsIgnoreCase("Drinks")) {
+                List<Ingredients> ingredients=new ArrayList<>();
+                List<Steps> steps=new ArrayList<>();
+                for (DataSnapshot ds2: ds.child("ingredients").getChildren())
+                {
+                    Ingredients ingredients1=new Ingredients(ds2.child("name").getValue(String.class),ds2.child("quantity").getValue(long.class),ds2.child("unitOfMeasure").getValue(String.class));
+                    ingredients.add(ingredients1);
+                }
+
+                for (DataSnapshot ds2: ds.child("preparationSteps").getChildren())
+                {
+                    Steps s=new Steps(ds2.child("description").getValue(String.class));
+                    steps.add(s);
+                }
+
+                if (currentUser!=null && name!=null && currentUser.equalsIgnoreCase(name))
+                {
+                    isPublishedByUser=true;
+                }
+                else {
+                    isPublishedByUser=false;
+                }
+                RecipeInfo recipe = new RecipeInfo(ds.child("title").getValue(String.class), ds.child("category").getValue(String.class), ds.child("picUri").getValue(String.class), ingredients, steps, ds.child("recipeId").getValue(String.class), ds.child("timestamp").getValue(String.class), name,isPublishedByUser,false);
+                recipieList.add(recipe);
+
+            }
+
+        }
     }
 }
