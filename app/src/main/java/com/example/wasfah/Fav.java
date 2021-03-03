6c1248b03c0f164225bf3df67ff6faa92a98cdb9
Hadeel20 <52760296+Hadeel@users.noreply.github.com>
2021-03-04 00:52:40 +0300

    Favorite Fragment

--- app/src/main/java/com/example/wasfah/Fav.java
+++ app/src/main/java/com/example/wasfah/Fav.java
@@ -12,11 +12,9 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.wasfah.model.RecipeModel;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,9 +22,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import com.squareup.picasso.Picasso;
-
-import java.util.ArrayList;
-import java.util.List;
 
 ///**
 // * A simple {@link Fragment} subclass.
@@ -40,23 +35,23 @@ public class Fav extends Fragment {
     private View RootView;
     private RecyclerView myFavList;
 
-    List<RecipeInfo> recipeList;
-    RecyclerView recyclerView;
-    RecyclerViewAdapter recyclerViewAdapter;
-    private String name;
-    private String email;
+//    List<RecipeInfo> recipeList;
+//    RecyclerView recyclerView;
+//    RecyclerViewAdapter recyclerViewAdapter;
+//    private String name;
+//    private String email;
 
     //Firebase
     DatabaseReference favRef;
-    DatabaseReference userRef;
+    DatabaseReference recRef;
     FirebaseAuth mAuth;
-    private String currentUserId;
+    private String recipeID;
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("Users");
-    DatabaseReference recipeRef = database.getReference("Recipes");
-    //FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-    //String userID = user.getUid();
+//    DatabaseReference myRef = database.getReference("Users");
+//    DatabaseReference recipeRef = database.getReference("Recipes");
+//    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//    String userID = user.getUid();
 
 
     @Override
@@ -65,74 +60,13 @@ public class Fav extends Fragment {
         // Inflate the layout for this fragment
         RootView = inflater.inflate(R.layout.fragment_fav, container, false);
 
-        myFavList = (RecyclerView) RootView.findViewById(R.id.fav_view);
+        myFavList = (RecyclerView) RootView.findViewById(R.id.rec_view);
         myFavList.setLayoutManager(new LinearLayoutManager(getContext()));
 
         mAuth = FirebaseAuth.getInstance();
-        currentUserId = mAuth.getCurrentUser().getUid();
-        favRef = database.getReference().child("FavoriteList").child(currentUserId);
-        userRef = database.getReference().child("Users");
-
-//        recipeList = new ArrayList<>();
-
-//        if (user != null) {
-//            myRef.child(userID).addValueEventListener(new ValueEventListener() {
-//                @Override
-//                public void onDataChange(DataSnapshot dataSnapshot) {
-//                    // This method is called once with the initial value and again
-//                    // whenever data at this location is updated.
-//
-//                    name = dataSnapshot.child("name").getValue(String.class);
-//                    email= dataSnapshot.child("email").getValue(String.class);
-////                        img =dataSnapshot.child("uimage").getValue(String.class);
-////                        if (img !=null){
-////                            Picasso.get().load(img).into(imageView);
-////                        }
-//
-////                    if (name != null) {
-////                        nameTv.setText(name);
-////                    }
-//
-//                }
-//
-//                @Override
-//                public void onCancelled(DatabaseError error) {
-//                    // Failed to read value
-////                    Log.w(TAG, "Failed to read value.", error.toException());
-//                }
-//            });
-//
-//            recipeRef.addValueEventListener(new ValueEventListener() {
-//                @Override
-//                public void onDataChange(DataSnapshot dataSnapshot) {
-//                    getUpdate(dataSnapshot);
-//                    RecyclerView rs=(RecyclerView) RootView.findViewById(R.id.rv);
-//                    if (recipeList.size()>0)
-//                    {
-//                        RecyclerViewAdapter recAdap = new RecyclerViewAdapter(getContext(),recipeList,name);
-//                        rs.setLayoutManager(new GridLayoutManager(getContext(),3));
-//                        rs.setAdapter(recAdap);
-//                    }
-//                    else {
-//                        rs.setVisibility(View.GONE);
-//                        TextView tv_noRecipe=(TextView) RootView.findViewById(R.id.tv_noRecipe);
-//                        tv_noRecipe.setVisibility(View.VISIBLE);
-//
-//
-//                    }
-//
-//                }
-//
-//                @Override
-//                public void onCancelled(DatabaseError error) {
-//                    // Failed to read value
-////                    Log.w(TAG, "Failed to read value.", error.toException());
-//                }
-//            });
-//
-//
-//
-//        }
+        recipeID = mAuth.getCurrentUser().getUid();
+        favRef = database.getReference().child("FavoriteList").child(recipeID);
+        recRef = database.getReference().child("Recipes");
 
         return RootView;
 
@@ -142,12 +76,12 @@ public class Fav extends Fragment {
     public void onStart() {
         super.onStart();
 
-        FirebaseRecyclerOptions options = new FirebaseRecyclerOptions.Builder<RecipeModel>().setQuery(favRef, RecipeModel.class).build();
-        FirebaseRecyclerAdapter<RecipeModel, favViewHolder> adapter = new FirebaseRecyclerAdapter<RecipeModel, favViewHolder>(options) {
+        FirebaseRecyclerOptions options = new FirebaseRecyclerOptions.Builder<RecipeInfo>().setQuery(favRef, RecipeInfo.class).build();
+        FirebaseRecyclerAdapter<RecipeInfo, favViewHolder> adapter = new FirebaseRecyclerAdapter<RecipeInfo, favViewHolder>(options) {
             @Override
-            protected void onBindViewHolder(@NonNull favViewHolder favViewHolder, int i, @NonNull RecipeModel recipeModel) {
-                String recipeId = getRef(i).getKey();
-                favRef.child(recipeId).addValueEventListener(new ValueEventListener() {
+            protected void onBindViewHolder(@NonNull favViewHolder favViewHolder, int i, @NonNull RecipeInfo recipeInfo) {
+                String recipesId = getRef(i).getKey();
+                favRef.child(recipesId).addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         if(snapshot.hasChild("picUri")) {
@@ -196,32 +130,4 @@ public class Fav extends Fragment {
     }
 
 
-//    private void getUpdate(@NonNull DataSnapshot snapshot) {
-//        if (recipeList != null && recipeList.size() > 0) {
-//            recipeList.clear();
-//        }
-//
-//        for (DataSnapshot ds : snapshot.getChildren()) {
-//            String email2 = snapshot.child(ds.getKey()).child("createdBy").getValue(String.class);
-//            if (email2 != null && email != null && email2.equalsIgnoreCase(email)) {
-//                List<Ingredients> ingredients=new ArrayList<>();
-//                List<Steps> steps=new ArrayList<>();
-//                for (DataSnapshot ds2: ds.child("ingredients").getChildren())
-//                {
-//                    Ingredients ingredients1=new Ingredients(ds2.child("name").getValue(String.class),ds2.child("quantity").getValue(long.class),ds2.child("unitOfMeasure").getValue(String.class));
-//                    ingredients.add(ingredients1);
-//                }
-//
-//                for (DataSnapshot ds2: ds.child("preparationSteps").getChildren())
-//                {
-//                    Steps s=new Steps(ds2.child("description").getValue(String.class));
-//                    steps.add(s);
-//                }
-//
-//                RecipeInfo recipe = new RecipeInfo(ds.child("title").getValue(String.class), ds.child("category").getValue(String.class), ds.child("picUri").getValue(String.class),ingredients,steps,ds.child("recipeId").getValue(String.class),ds.child("timestamp").getValue(String.class));
-//                recipeList.add(recipe);
-//            }
-//
-//        }
-//    }
 }
